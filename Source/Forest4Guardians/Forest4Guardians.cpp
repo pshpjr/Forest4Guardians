096f6baa4aedf753ba3f83a1ec4ef68cb7fd226b
SeongHawnPark <pshpjr@naver.com>
2022-05-28 17:43:32 +0900

    패키징, 라이브코딩 문제 없는 거 확인.
    폴더 구조 정리

---
+++ Source/Forest4Guardians/Forest4Guardians.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Forest4Guardians.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Forest4Guardians, "Forest4Guardians" );
